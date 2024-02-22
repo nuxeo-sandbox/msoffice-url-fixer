6e39ddeb39250f8def3208582c8e5ec5ddec0989
Vincent Dutat <vdutat@nuxeo.com>
2024-02-21 16:54:15 -0800

    Configurable UI url segment

--- msoffice-url-fixer-core/src/main/java/org/nuxeo/ecm/platform/ui/web/MsOfficeUrlFilter.java
+++ msoffice-url-fixer-core/src/main/java/org/nuxeo/ecm/platform/ui/web/MsOfficeUrlFilter.java
@@ -1,8 +1,12 @@
 package org.nuxeo.ecm.platform.ui.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -23,23 +27,34 @@ public class MsOfficeUrlFilter implements Filter {
 
 	private static final String CONTEXT_PATH_PROPERTY = "org.nuxeo.ecm.contextPath";
 
+    private static final String CUSTOM_WEBUI_CONTEXT_PATH_PROPERTY = "msofficeurlfixer.webui.contextPath";
+
 	private Map<String, String> urlFragmentPatterns = new HashMap<>();
 	
-	private String newUrl;
-
+	private List<String> webUiUrlFragmentPatterns = Stream.of("ui").collect(Collectors.toList());;
+	
 	public MsOfficeUrlFilter() {
 	}
 
 	@Override
 	public void init(FilterConfig fConfig) throws ServletException {
 		log.debug("MsOfficeUrlFilter initialization");
-		String contextRoot = Framework.getProperty(CONTEXT_PATH_PROPERTY);
-		var replacementFragment = String.format("%s/ui/#!", contextRoot);
-		if (urlFragmentPatterns.isEmpty()) {
-		    urlFragmentPatterns.put(String.format("%s/ui/%%23!", contextRoot), replacementFragment);
-		    urlFragmentPatterns.put(String.format("%s/ui/#%%21", contextRoot), replacementFragment);
-		    urlFragmentPatterns.put(String.format("%s/ui/%%23%%21", contextRoot), replacementFragment);
-		}
+        if (urlFragmentPatterns.isEmpty()) {
+            String contextRoot = Framework.getProperty(CONTEXT_PATH_PROPERTY);
+            String webuiContextRoot = Framework.getProperty(CUSTOM_WEBUI_CONTEXT_PATH_PROPERTY);
+            if (webuiContextRoot != null) {
+                for (var uiPattern : Arrays.stream(webuiContextRoot.split(",")).map(String::trim).collect(Collectors.toList())) {
+                    log.debug("<init> UI url: {}", uiPattern);
+                    webUiUrlFragmentPatterns.add(uiPattern);
+                }
+            }
+            for (var uiPattern : webUiUrlFragmentPatterns) {
+                var replacementFragment = String.format("%s/%s/#!", contextRoot, uiPattern);
+                urlFragmentPatterns.put(String.format("%s/%s/%%23!", contextRoot, uiPattern), replacementFragment);
+                urlFragmentPatterns.put(String.format("%s/%s/#%%21", contextRoot, uiPattern), replacementFragment);
+                urlFragmentPatterns.put(String.format("%s/%s/%%23%%21", contextRoot, uiPattern), replacementFragment);
+            }
+        }
 	}
 
 	@Override
@@ -52,20 +67,17 @@ public class MsOfficeUrlFilter implements Filter {
 		if (request instanceof HttpServletRequest) {
 			HttpServletRequest req = (HttpServletRequest) request;
 			String url = req.getRequestURL().toString();
-			newUrl = null;
-			urlFragmentPatterns.forEach((pattern, replacement) -> {
-			    if (newUrl == null) {
-			        log.debug("<doFilter> {} == {} == {}", url, pattern, replacement);
-			        if (url.indexOf(pattern) != -1) {
-			            newUrl = url.replaceFirst(pattern, replacement);
-			            log.debug("<doFilter> Redirection to {}", newUrl);
-			        }
-			    }
-			});
-			if (newUrl != null) {
-			    HttpServletResponse httpResponse = (HttpServletResponse) response;
-			    httpResponse.sendRedirect(newUrl);
-			    return;
+			for (var entry : urlFragmentPatterns.entrySet()) {
+			    var pattern = entry.getKey();
+			    var replacement = entry.getValue();
+                log.debug("<doFilter> {} == {} == {}", url, pattern, replacement);
+                if (url.indexOf(pattern) != -1) {
+                    var newUrl = url.replaceFirst(pattern, replacement);
+                    log.debug("<doFilter> Redirection to {}", newUrl);
+                    HttpServletResponse httpResponse = (HttpServletResponse) response;
+                    httpResponse.sendRedirect(newUrl);
+                    return;
+                }
 			}
 		}
 		chain.doFilter(request, response);
